7827576230f81f70b9f70c1b41f62e61ead3b7a4
Kumaravel187 <46121794+Kumaravel187@users.noreply.github.com>
2023-08-21 17:20:14 +0800

    Completed challenge
    - Added in complete javascript for date logic
    - Refractored code to use DRY methodology
    - Added in comments for html, styling and js files

--- app/app.js
+++ app/app.js
@@ -1,109 +1,122 @@
+// Get the button element and input elements with class "input"
 const btn = document.getElementById("btn");
 const inputEls = document.querySelectorAll(".input");
 
+// Get individual input elements for day, month, and year
 const dayInputElement = document.getElementById("day");
 const monthInputElement = document.getElementById("month");
 const yearInputElement = document.getElementById("year");
 
+// Get user input elements for day, month, and year
 const dayUserInput = document.getElementById("dayInput");
 const monthUserInput = document.getElementById("monthInput");
 const yearUserInput = document.getElementById("yearInput");
 
+// Function to show an error message for an input element
+function showErrorMessage(inp) {
+  inp.classList.add("error-outline");
+  inp.classList.remove("success-outline");
+  inp.previousElementSibling.classList.add("error");
+  inp.nextElementSibling.classList.add("error");
+}
+
+// Function to show a success message for an input element
+function showSuccessMessage(inp) {
+  inp.classList.add("success-outline");
+  inp.classList.remove("error-outline");
+  inp.previousElementSibling.classList.remove("error");
+  inp.nextElementSibling.textContent = "";
+}
+
+// Function to validate a single input element
 function validateInput(input) {
   const inputValue = input.value.trim();
 
   if (inputValue === "") {
-    input.classList.add("error-outline");
-    input.classList.remove("success-outline");
-    input.nextElementSibling.textContent = `This field is required.`;
-    input.previousElementSibling.classList.add("error");
+    showErrorMessage(input);
+    input.nextElementSibling.textContent = `This field is required!`;
     return false;
   } else {
-    input.classList.add("success-outline");
-    input.classList.remove("error-outline");
-    input.nextElementSibling.textContent = "";
-    input.previousElementSibling.classList.remove("error");
+    showSuccessMessage(input);
     return true;
   }
 }
 
+// Function to validate all input elements and display error messages if necessary
 function validateAllInputs() {
   let allInputsValid = true;
 
+  // Loop through each input element
   inputEls.forEach(function (input) {
     if (!validateInput(input)) {
-      allInputsValid = false;
+      allInputsValid = false; // Mark as invalid if any input is not valid
     }
   });
 
   return allInputsValid;
 }
 
+// Function to validate the day input element
 function validateDay(day) {
   const dayValue = Number(day.value);
   if (dayValue >= 1 && dayValue <= 31) {
-    day.classList.remove("error-outline");
-    day.classList.add("success-outline");
-    day.previousElementSibling.classList.remove("error");
-
+    showSuccessMessage(day);
     return dayValue;
   } else {
-    let dayInputElName = day.name;
-    day.classList.add("error-outline");
-    day.classList.remove("success-outline");
+    showErrorMessage(day);
+    const dayInputElName = day.name;
     day.nextElementSibling.textContent = `Must be a valid ${dayInputElName}!`;
-    day.previousElementSibling.classList.add("error");
   }
 }
 
+// Function to validate the month input element
 function validateMonth(month) {
   const monthValue = Number(month.value);
   if (monthValue >= 0 && monthValue <= 12) {
-    month.classList.remove("error-outline");
-    month.classList.add("success-outline");
-    month.previousElementSibling.classList.remove("error");
+    showSuccessMessage(month);
     return monthValue;
   } else {
-    let monthInputElName = month.name;
-    month.classList.add("error-outline");
-    month.classList.remove("success-outline");
-    month.previousElementSibling.classList.add("error");
+    showErrorMessage(month);
+    const monthInputElName = month.name;
     month.nextElementSibling.textContent = `Must be a valid ${monthInputElName}!`;
   }
 }
 
+// Function to validate the year input element
 function validateYear(year) {
   const yearValue = Number(year.value);
   if (yearValue >= 1950 && yearValue <= 2023) {
-    year.classList.remove("error-outline");
-    year.classList.add("success-outline");
-    year.previousElementSibling.classList.remove("error");
+    showSuccessMessage(year);
     return yearValue;
   } else {
-    let yearInputElName = year.name;
-    year.classList.add("error-outline");
-    year.classList.remove("success-outline");
-    year.previousElementSibling.classList.add("error");
+    showErrorMessage(year);
+    const yearInputElName = year.name;
     year.nextElementSibling.textContent = `Must be a valid ${yearInputElName}!`;
   }
 }
 
+// Add a click event listener to the button
 btn.addEventListener("click", function () {
+  // Validate all input elements before proceeding
   if (!validateAllInputs()) {
-    return;
+    return; // Stop execution if any input is invalid
   }
 
+  // Validate individual day, month, and year inputs
   const day = validateDay(dayInputElement);
   const month = validateMonth(monthInputElement);
   const year = validateYear(yearInputElement);
 
+  // Get the current date
   const currentDate = new Date();
 
-  const currentday = currentDate.getDate();
-  const currentMonth = currentDate.getMonth() + 1 + 12;
-  const currentYear = currentDate.getFullYear();
+  // Get current day, month, and year
+  const currentDay = currentDate.getDate();
+  const currentMonth = currentDate.getMonth() + 1 + 12; // Corrected for 0-based index
+  const currentYear = currentDate.getFullYear() - 1;
 
-  dayUserInput.textContent = currentday - day;
+  // Calculate and display the differences in day, month, and year
+  dayUserInput.textContent = currentDay - day;
   monthUserInput.textContent = currentMonth - month;
   yearUserInput.textContent = currentYear - year;
 });
